1022f37fb2ceb167a08ac1af84758cad397f552a
Mark L. Walker <mlwtlw22@gmail.com>
2017-02-27 07:25:45 -0700

    Corrected expectations in the other tests. NOTE: I was unable to implement an expectation that load() had been called without having the subsequent expectations fail. Logging the books array length returns zero with this expectation enabled.

--- src/app/shared/library.service.spec.ts
+++ src/app/shared/library.service.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-unused-variable */
 
-import {TestBed, async, inject} from '@angular/core/testing';
+import {TestBed, async, inject, tick} from '@angular/core/testing';
 import {LibraryService} from './library.service';
 import {Book} from "./book";
 
@@ -40,46 +40,48 @@ describe('LibraryService', () => {
   });
 
   it('can remove a book to the library', () => {
-    expect(libraryService.books.length == 0);
+    expect(libraryService.books.length).toBe(0);
     let bookToRemove = createBookFixture("rembook-1");
     libraryService.addBook(bookToRemove);
-    expect(libraryService.books.length == 1);
-    expect(libraryService.hasBook(bookToRemove) == true);
+    expect(libraryService.books.length).toBe(1);
+    expect(libraryService.hasBook(bookToRemove)).toBe(true);
     libraryService.removeBook(bookToRemove);
-    expect(libraryService.hasBook(bookToRemove) == false);
+    expect(libraryService.hasBook(bookToRemove)).toBe(false);
   });
 
   it('checks if a book is already in the library', () => {
-    expect(libraryService.books.length == 0);
+    expect(libraryService.books.length).toBe(0);
     let testBook = createBookFixture("hasbook-1");
-    expect(libraryService.hasBook(testBook) == false);
-    libraryService.addBook(createBookFixture(testBook));
-    expect(libraryService.books.length == 1);
-    expect(libraryService.hasBook(testBook) == true);
+    expect(libraryService.hasBook(testBook)).toBe(false);
+    libraryService.addBook(testBook);
+    expect(libraryService.books.length).toBe(1);
+    expect(libraryService.hasBook(testBook)).toBe(true);
   });
 
   it('can save and load the books', () => {
-    expect(libraryService.books.length == 0);
+    expect(libraryService.books.length).toBe(0);
     let bookToAdd1 = createBookFixture("addbook-1");
-    libraryService.addBook(createBookFixture(bookToAdd1));
+    libraryService.addBook(bookToAdd1);
     let bookToAdd2 = createBookFixture("addbook-2");
-    libraryService.addBook(createBookFixture(bookToAdd2));
+    libraryService.addBook(bookToAdd2);
     let bookToAdd3 = createBookFixture("addbook-3");
-    libraryService.addBook(createBookFixture(bookToAdd3));
-    expect(libraryService.books.length == 3);
+    libraryService.addBook(bookToAdd3);
+    expect(libraryService.books.length).toBe(3);
     spyOn(libraryService, 'save');
     libraryService.save();
-    expect(libraryService.save).toHaveBeenCalled()
+    expect(libraryService.save).toHaveBeenCalled();
     libraryService.removeBook(bookToAdd1);
     libraryService.removeBook(bookToAdd2);
     libraryService.removeBook(bookToAdd3);
-    expect(libraryService.books.length == 0);
-    spyOn(libraryService, 'load').and.returnValue(undefined);
+    expect(libraryService.books.length).toBe(0);
+    // spyOn(libraryService, 'load');
     libraryService.load();
-    expect(libraryService.load).toHaveBeenCalled()
-    expect(libraryService.books.length == 3);
-    expect(libraryService.hasBook(bookToAdd1) == true);
-    expect(libraryService.hasBook(bookToAdd2) == true);
-    expect(libraryService.hasBook(bookToAdd3) == true);
+    // console.log("... post-load ...");
+    // console.log("books.length = " + libraryService.books.length);
+    // expect(libraryService.load).toHaveBeenCalled();
+    expect(libraryService.hasBook(bookToAdd1)).toBe(true);
+    expect(libraryService.hasBook(bookToAdd2)).toBe(true);
+    expect(libraryService.hasBook(bookToAdd3)).toBe(true);
+    expect(libraryService.books.length).toBe(3);
   });
 });
