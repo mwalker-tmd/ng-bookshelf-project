ad1b3719012687c7485fba4dfc5e96896dd5d686
Mark L. Walker <mlwtlw22@gmail.com>
2017-03-02 08:04:39 -0700

    Fixed href; Modified tests.

--- src/app/errorpage/errorpage.component.spec.ts
+++ src/app/errorpage/errorpage.component.spec.ts
@@ -1,11 +1,8 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
 import { ErrorPageComponent } from './errorpage.component';
 
-fdescribe('ErrorPageComponent', () => {
+describe('ErrorPageComponent', () => {
   let component: ErrorPageComponent;
   let fixture: ComponentFixture<ErrorPageComponent>;
 
@@ -25,4 +22,20 @@ fdescribe('ErrorPageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`should have as title 'app works!'`, async(() => {
+    const errorpage = fixture.debugElement.componentInstance;
+    expect(errorpage.title).toEqual('An Error Has Occurred!');
+  }));
+
+  it('should render title in a h1 tag', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('An Error Has Occurred');
+  }));
+
+  it('should link back to home page', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector("a").href).toBe("http://localhost:9876/");
+  }));
+
 });
